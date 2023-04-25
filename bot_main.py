97e80a9cbf1479a82938b948f4f4dc2130f08cf5
Sebastian Jung <see.bastian@t-online.de>
2023-04-26 00:50:42 +0200

    configurated logging from the discord module

--- bot_main.py
+++ bot_main.py
@@ -1,8 +1,26 @@
 import discord
 from discord.ext import tasks, commands
 import os
+import logging, coloredlogs
 
 import tools
+
+
+FORMATTER = logging.Formatter(tools.LOG_FORMAT)
+
+DISCORD_LOGGER = logging.getLogger("discord")
+DISCORD_LOGGER.setLevel(logging.INFO)
+logging.getLogger("discord.http").setLevel(logging.INFO)
+
+DISCORD_HANDLER = logging.FileHandler("log.log")
+DISCORD_HANDLER.setFormatter(FORMATTER)
+
+DISCORD_STREAM_HANDLER = logging.StreamHandler()
+DISCORD_STREAM_HANDLER.setLevel(logging.DEBUG)
+DISCORD_STREAM_HANDLER.setFormatter(coloredlogs.ColoredFormatter(tools.LOG_FORMAT))
+
+DISCORD_LOGGER.addHandler(DISCORD_HANDLER)
+DISCORD_LOGGER.addHandler(DISCORD_STREAM_HANDLER)
 
 
 intents = discord.Intents.all()
@@ -18,15 +36,15 @@ async def on_ready():
             if filename.endswith('.py'):
                 # remove '.py' and load extension
                 bot.load_extension(f'cogs.{filename[:-3]}')
-                tools.logger.info(f'Loaded Module {filename}')
+                tools.LOGGER.info(f'Loaded Module {filename}')
 
-        print("loaded all cogs")                
+        tools.LOGGER.info("loaded all cogs")                
 
     except Exception as e:
-        tools.logger.exception(e)
+        tools.LOGGER.exception(e)
 
 
 if __name__ == "__main__":
     # get discord api token and run bot
     discord_token = tools.get_json("secrets.json")['DISCORD']['BOT_TOKEN']
-    bot.run(discord_token)
+    bot.run(discord_token, log_handler=None)
