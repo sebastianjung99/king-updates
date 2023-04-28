1ade93e1dc54751b35277e33d0a13b07017822c4
Sebastian Jung <see.bastian@t-online.de>
2023-04-29 00:37:57 +0200

    added function to validate tokens and request a new one if invalid

--- tokens.py
+++ tokens.py
@@ -39,3 +39,35 @@ def get_tokens():
         "OAUTH_SECRET": res[2]['oauth_secret']
     }
 }
+
+
+def check_tokens():
+    """
+    Checks twitch oauth (user) token. Requests and stores a new one if old one is expired 
+    or about to.
+    """
+
+    headers = {'Authorization': f"OAuth {SECRETS['TWITCH']['OAUTH']}"}
+    r = requests.get('https://id.twitch.tv/oauth2/validate', headers=headers)
+    # check if token is valid
+    if r.status_code == 200:
+        tools.LOGGER.info("Twitch user token validated.")
+    # token not valid, request new one
+    elif r.status_code == 401:
+        # get new token
+        r = requests.post(f"https://id.twitch.tv/oauth2/token?grant_type=client_credentials&client_id={SECRETS['TWITCH']['CLIENT']}&client_secret={SECRETS['TWITCH']['SECRET']}")
+        r = r.json()
+        oauth_new = r['access_token']
+
+        # store new token
+        mariadb_connection, cursor = tools.get_db_connection()
+        sql_statement = "UPDATE secrets SET oauth = %s WHERE app = %s"
+        values = (oauth_new, 'twitch')
+        cursor.execute(sql_statement, values)
+        mariadb_connection.commit()
+        mariadb_connection.close()
+        
+        tools.LOGGER.info("Aquired new Twitch user token.")
+    # something went wrong
+    else:
+        tools.LOGGER.error(f"Error checking Twitch user token: {r.text}")
